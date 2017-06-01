de6db4589f51f3fbf27b27c6c2120f59a79946c3
Adrián Rojas <rojasadrian12@gmail.com>
2017-06-01 22:09:34 +0200

    update

--- js/noticias.js
+++ js/noticias.js
@@ -1,4 +1,24 @@
-/*$(function() {
+//---------------Movimiento header-----------------------
+
+function openTab(tabName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(tabName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+document.getElementById("active").click();
+
+
+//-------------------JSON-------------------------------
+
+$(function() {
 
     var i = 0;
 
@@ -53,7 +73,7 @@ function ponerNoticias(json) {
             '</div>' +
             '</div>',
     });
-};*/
+};
 
 
 jQuery(document).ready(function() {
@@ -73,20 +93,3 @@ jQuery(document).ready(function() {
         return false;
     })
 });
-
-//---------------Movimiento header-----------------------
-
-function openTab(tabName, elmnt, color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(tabName).style.display = "block";
-    elmnt.style.backgroundColor = color;
-}
-document.getElementById("active").click();
