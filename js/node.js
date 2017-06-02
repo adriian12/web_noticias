4d71cb1df974d45384f5830429ae91e3bab0d768
Adrián Rojas <adriian12@users.noreply.github.com>
2017-06-02 16:43:50 +0200

    Add files via upload

---
+++ js/node.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.go-top').fadeIn(duration);
+        } else {
+            jQuery('.go-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.go-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
+
+// function openTab(tabName, elmnt, color) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablink");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].style.backgroundColor = "";
+//     }
+//     document.getElementById(tabName).style.display = "block";
+//     elmnt.style.backgroundColor = color;
+// }
+// document.getElementById("active").click();
--- js/noticias.js
+++ js/noticias.js
@@ -1,81 +1,4 @@
-//---------------Movimiento header-----------------------
-
-function openTab(tabName, elmnt, color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(tabName).style.display = "block";
-    elmnt.style.backgroundColor = color;
-}
-document.getElementById("active").click();
-
-
-//-------------------JSON-------------------------------
-
-$(function() {
-
-    var i = 0;
-
-    function cargarJSON(i) {
-        fichero = "data/" + i + "noticias.json";
-        $.getJSON(fichero, function(jsonObject) {
-            ponerNoticias(jsonObject);
-
-        });
-
-
-    }
-
-    $("#target").click(function() {
-        cargarJSON(i + 1);
-        cargarJSON(i + 2);
-        i = 3;
-    });
-
-
-    $(document).scroll(function() {
-        if (($(window).scrollTop() + $(window).height() > $(document).height() - 10) && (i < 2)) {
-            cargarJSON(i + 1);
-            i++;
-        }
-    });
-
-});
-
-
-function ponerNoticias(json) {
-    $.each(json, function(j, objeto) {
-        $('.cargarNoticias').append('<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 noticias"' + 
-            '<div class="card">' +
-            '<a href="#">' + objeto.img +
-            '<h2>' + objeto.titulo + '.</h2>' +
-            '</a>' +
-            '<div class="datetime">' + 
-            '<span class="glyphicon glyphicon-calendar">' +
-            '</span>' +
-            '<span>' + objeto.fecha + '</span>' +
-            '</div>' +
-            '<div class="card-content">' +
-            '<h3>' + objeto.titulo2 + '</h3>' +
-            '<p>' + objeto.texto + '</p>' +
-            '<div id="#' + objeto.id +  '" class="collapse">' +
-            '<p>' + objeto.texo2 + '</p>' +
-            '</div>' + 
-            '</div>' +
-            '<div class="card-read-more">' +
-            '<a class="btn btn-link btn-block" data-toggle="collapse" href="#' + objeto.id + '" onclick="cambiar()">' + objeto.mas + '</a>' +
-            '</div>' +
-            '</div>',
-    });
-};
-
-
+//------------------Volver arriba---------------------------
 jQuery(document).ready(function() {
     var offset = 220;
     var duration = 500;
@@ -93,3 +16,18 @@ jQuery(document).ready(function() {
         return false;
     })
 });
+//---------------Movimiento header-----------------------
+function openTab(tabName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(tabName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+document.getElementById("active").click();
